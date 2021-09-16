4be838b2be9f774777ac113af93a6ee6c3bb30d9
Jon Dayley <jonathandayley@gmail.com>
2021-09-16 11:21:24 -0600

    Trim argument to validate  script

--- validate.sh
+++ validate.sh
@@ -3,14 +3,17 @@
 # Set prefix
 pre="mxc: "
 
+# Trim whitespace from path
+f=$(echo $1 | sed "s/ *$//g" | sed "s/^ *//g")
+
 # Check whether parameter is a file
-[[ -f $1 ]] || {
+[[ -f $f ]] || {
 	echo $pre"Buffer not saved"
 	exit 1
 }
 
 # Check whether parameter is executable
-[[ -x $1 ]] || {
+[[ -x $f ]] || {
 	echo $pre"$1 is not executable"
 	exit 1
 }
